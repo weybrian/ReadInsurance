1e8b0fbbf70618599b01915c6e82958bc2a950fb
wey <weybrian@gmail.com>
2024-12-12 22:01:32 +0800

    add Q1.xlsx

---
+++ Readme.txt
@@ -0,0 +1,7 @@
+執行方式：
+將 target/test-0.0.1-SNAPSHOT.jar 與 Q1.xlsx 放在同一個目錄下
+在此目錄下執行終端機，輸入 java -jar test-0.0.1-SNAPSHOT.jar
+按下 enter，即可獲取結果
+
+注意：
+只會讀取檔名為 Q1.xlsx 的 第一個工作表
--- src/main/java/gainMiles/ReadInsurance.java
+++ src/main/java/gainMiles/ReadInsurance.java
@@ -1,67 +1,177 @@
 package gainMiles;
 
 import java.io.FileInputStream;
-import java.util.Iterator;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.BuiltinFormats;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ReadInsurance {
 
 	public static void main(String[] args) {
-		read("/Users/weybrian/Downloads/Q1.xlsx");
+		List<Data> dataList = read("Q1.xlsx");
+		printData(dataList);
+	}
+
+	/**
+	 * 列印資料
+	 * 
+	 * @param dataList
+	 */
+	private static void printData(List<Data> dataList) {
+		System.out.println("[Benefit          , Coverage   , Category             , Plan Name, Coverage Value]");
+		System.out.println("==================================================================================");
+		for (Data data : dataList) {
+			System.out.println(data.toString());
+		}
 	}
 
 	/**
 	 * 讀取檔案
+	 * 
 	 * @param string
 	 */
-	private static void read(String filepath) {
-		try
-        {
-            FileInputStream file = new FileInputStream(filepath);
- 
-            //Create Workbook instance holding reference to .xlsx file
-            XSSFWorkbook wb = new XSSFWorkbook(file);
- 
-            //Get first/desired sheet from the workbook
-            XSSFSheet ws = wb.getSheetAt(0);
- 
-            //Iterate through each rows one by one
-            Iterator<Row> rowIterator = ws.iterator();
-            while (rowIterator.hasNext()) 
-            {
-                Row row = rowIterator.next();
-                //For each row, iterate through all the columns
-                Iterator<Cell> cellIterator = row.cellIterator();
-                 
-                while (cellIterator.hasNext()) 
-                {
-                    Cell cell = cellIterator.next();
-                    //Check the cell type and format accordingly
-                    switch (cell.getCellType()) 
-                    {
-                        case NUMERIC:
-                            System.out.print(cell.getNumericCellValue());
-                            break;
-                        case STRING:
-                            System.out.print(cell.getStringCellValue());
-                            break;
-					default:
-						break;
-                    }
-                }
-                System.out.println("Reading File Completed.");
-            }
-            wb.close();
-            file.close();
-        } 
-        catch (Exception ex) 
-        {
-            ex.printStackTrace();
-        }
+	private static List<Data> read(String filepath) {
+		List<Data> dataList = new ArrayList<Data>();
+		try (FileInputStream fis = new FileInputStream(filepath); XSSFWorkbook workbook = new XSSFWorkbook(fis)) {
+
+			XSSFSheet inputSheet = workbook.getSheetAt(0);
+
+			// 用來計算 plan 數量
+			int lastCellNum = 0;
+			ArrayList<String> planList = new ArrayList<String>();
+			Data dataBean = new Data();
+			// 取資料
+			for (int i = 0; i < inputSheet.getLastRowNum(); i++) {
+				// 取 plan name
+				if (i == 0) {
+					XSSFRow row0 = inputSheet.getRow(i);
+					lastCellNum = row0.getLastCellNum();
+					for (int j = 5; j < lastCellNum; j++) {
+						planList.add(row0.getCell(j).getStringCellValue());
+					}
+					continue;
+				}
+
+				if (inputSheet.getRow(i) == null) {
+					continue;
+				}
+				XSSFCell cell0 = inputSheet.getRow(i).getCell(0);
+				XSSFCell cell1 = inputSheet.getRow(i).getCell(1);
+				if (cell0 != null) {
+					if (cell1 == null) {// benefit
+						dataBean.setBenefit(cell0.getStringCellValue());
+					} else {// coverage & data
+						dataBean.setCoverage(cell0.getStringCellValue());
+						dataBean.setCategory(cell1.getStringCellValue());
+						for (int j = 5; j < lastCellNum; j++) {
+							dataBean.setPlanName(planList.get(j - 5));
+							dataBean.setCoverageValue(getCell2String(inputSheet.getRow(i).getCell(j)));
+							dataList.add((Data) dataBean.clone());
+						}
+					}
+				} else {
+					if (cell1 != null) {// data
+						dataBean.setCategory(cell1.getStringCellValue());
+						for (int j = 5; j < lastCellNum; j++) {
+							dataBean.setPlanName(planList.get(j - 5));
+							dataBean.setCoverageValue(getCell2String(inputSheet.getRow(i).getCell(j)));
+							dataList.add((Data) dataBean.clone());
+						}
+					}
+				}
+			}
+		} catch (IOException e) {
+			System.err.println("發生錯誤: " + e.getMessage());
+			e.printStackTrace();
+		} catch (CloneNotSupportedException e) {
+			e.printStackTrace();
+		}
+		return dataList;
 	}
 
+	/**
+	 * 取 cell 成字串
+	 * @param cell
+	 * @return
+	 */
+	private static String getCell2String(XSSFCell cell) {
+		DataFormatter dataFormatter = new DataFormatter();
+		switch (cell.getCellType()) {
+		case NUMERIC:
+			return dataFormatter.formatCellValue(cell);
+		case STRING:
+			return cell.getStringCellValue();
+		default:
+			break;
+		}
+		return null;
+	}
+
+	/**
+	 * 資料 Bean
+	 */
+	static class Data implements Cloneable {
+		private String benefit;
+		private String coverage;
+		private String category;
+		private String planName;
+		private String coverageValue;
+
+		public String getBenefit() {
+			return benefit;
+		}
+
+		public void setBenefit(String benefit) {
+			this.benefit = benefit;
+		}
+
+		public String getCoverage() {
+			return coverage;
+		}
+
+		public void setCoverage(String coverage) {
+			this.coverage = coverage;
+		}
+
+		public String getCategory() {
+			return category;
+		}
+
+		public void setCategory(String category) {
+			this.category = category;
+		}
+
+		public String getPlanName() {
+			return planName;
+		}
+
+		public void setPlanName(String planName) {
+			this.planName = planName;
+		}
+
+		public String getCoverageValue() {
+			return coverageValue;
+		}
+
+		public void setCoverageValue(String coverageValue) {
+			this.coverageValue = coverageValue;
+		}
+
+		@Override
+		protected Object clone() throws CloneNotSupportedException {
+			return super.clone();
+		}
+
+		@Override
+		public String toString() {
+			return benefit + "," + coverage + "," + category + "," + planName + "," + coverageValue;
+		}
+	}
 }
